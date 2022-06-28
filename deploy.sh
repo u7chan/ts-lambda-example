c8969431895a4e138b0ff33043641cae86ee5a91
una <unagamix@gmail.com>
2022-06-29 00:41:41 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ fi
 AWS_LAMBDA_FUNCTION_NAME=$1
 AWS_LAMBDA_ROLE=$2
 
-# aws lambda delete-function --function-name $AWS_LAMBDA_FUNCTION_NAME 2>/dev/null
+aws lambda delete-function --function-name $AWS_LAMBDA_FUNCTION_NAME 2>/dev/null
 
-# aws lambda create-function \
-#   --function-name $AWS_LAMBDA_FUNCTION_NAME \
-#   --runtime "nodejs16.x" \
-#   --role $AWS_LAMBDA_ROLE \
-#   --zip-file "fileb://dist/index.zip" \
-#   --handler index.handler
+aws lambda create-function \
+  --function-name $AWS_LAMBDA_FUNCTION_NAME \
+  --runtime "nodejs16.x" \
+  --role $AWS_LAMBDA_ROLE \
+  --zip-file "fileb://dist/index.zip" \
+  --handler index.handler
