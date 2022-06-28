0e2ebf8daa42323c9ce3278397ba6d9d5e36b214
una <unagamix@gmail.com>
2022-06-29 00:41:22 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
+if [ -z "$1"] || [ -z "$2" ]; then
   echo "Usage: $0 [arguments]"
   echo "  \$1: Lambda Function Name"
   echo "  \$2: The Amazon Resource Name (ARN) of the function's execution role"
@@ -11,11 +11,11 @@ fi
 AWS_LAMBDA_FUNCTION_NAME=$1
 AWS_LAMBDA_ROLE=$2
 
-aws lambda delete-function --function-name $AWS_LAMBDA_FUNCTION_NAME 2>/dev/null
+# aws lambda delete-function --function-name $AWS_LAMBDA_FUNCTION_NAME 2>/dev/null
 
-aws lambda create-function \
-  --function-name $AWS_LAMBDA_FUNCTION_NAME \
-  --runtime "nodejs16.x" \
-  --role $AWS_LAMBDA_ROLE \
-  --zip-file "fileb://dist/index.zip" \
-  --handler index.handler
+# aws lambda create-function \
+#   --function-name $AWS_LAMBDA_FUNCTION_NAME \
+#   --runtime "nodejs16.x" \
+#   --role $AWS_LAMBDA_ROLE \
+#   --zip-file "fileb://dist/index.zip" \
+#   --handler index.handler
